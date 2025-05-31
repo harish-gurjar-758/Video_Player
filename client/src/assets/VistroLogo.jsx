c3fe168d9bb703680a4adfb791b3c642304f350f
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-31 11:50:11 +0530

    set the light logo

--- client/src/assets/VistroLogo.jsx
+++ client/src/assets/VistroLogo.jsx
@@ -1,22 +1,11 @@
 // src/components/VistroLogo.jsx
 import React from 'react';
+import lightThemeLogo from './logos/vistro_light_bg_logo.png'
 
 const VistroLogo = () => {
   return (
-    <div className="flex items-center space-x-2">
-      {/* Play button icon */}
-      <div className="bg-blue-500 rounded-md p-2 flex items-center justify-center">
-        <svg
-          className="w-5 h-5 text-white"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-        >
-          <path d="M6.5 5.5v9l7-4.5-7-4.5z" />
-        </svg>
-      </div>
-
-      {/* Brand text */}
-      <h1 className="text-2xl font-bold text-black">Vistro</h1>
+    <div>
+        <img src={lightThemeLogo} alt="" width={100} height={40}/>
     </div>
   );
 };
