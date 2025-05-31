fda96e97fd459fc4b1f8e822cef324493b6a6991
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-31 18:28:27 +0530

    add the more logics and secure the password

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/users.model.js";
+import bcrypt from 'bcryptjs';
+import { generateToken } from "../lib/utils.js";
 
 
 // Create a NEw Account
@@ -23,18 +25,42 @@ export const createAccount = async (req, res) => {
         }
 
         const user = await User.findOne({ email });
+        if (user) return res.status(400).json({
+            message: "Email already exists"
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
             username,
             email,
-            password
+            password: hashedPassword,
         });
 
-        await newUser.save();
+        if (newUser) {
+            // generate jwt token here
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                username: newUser.username,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+                channelBanner: newUser.channelBanner,
+            });
+
+        } else {
+            res.status(400).json({
+                message: "Invalid user data"
+            });
+        }
+
     } catch (error) {
         console.log("Error in Signup controller", error.message);
         res.status(500).json({
             message: "Internal Server Error"
         });
     }
-}
+};
