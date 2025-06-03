167597ee1134505498e8c655a3bfbeee103ccb6e
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-06-03 19:37:52 +0530

    create the login , Get Logged-in User and log out api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const BASIC_URL = "http://localhost:5000/api";
 
+// Create Account
+
 export const createAccountApi = async (userData) => {
   try {
     const response = await axios.post(`${BASIC_URL}/auth/create-account`, userData);
@@ -10,3 +12,46 @@ export const createAccountApi = async (userData) => {
     throw new Error(error.response?.data?.message || "Failed to create account.");
   }
 };
+
+// Login Account
+export const loginApi = async (userData) => {
+  try {
+    const response = await axios.post(`${BASIC_URL}/auth/login`, userData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Login failed.");
+  }
+};
+
+// Get Logged-in User
+
+export const getLoggedInUserApi = async () => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    throw new Error("No token found. Please log in.");
+  }
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+
+  try {
+    const response = await axios.get(`${BASIC_URL}/auth/me`, config);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "User not logged in.");
+  }
+};
+
+// Logout 
+
+export const logoutApi = async ()=>{
+  try {
+    await axios.post(`${BASIC_URL}/auth/logout`)
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Log out failed.");
+  }
+}
